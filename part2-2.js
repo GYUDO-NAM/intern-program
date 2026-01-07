9a9e1ba8fbe7542a45942a71f14e40e3c04ddd91
GyuDo-NAM <namgyudo49@gmail.com>
2026-01-07 18:54:16 +0900

    part2

---
+++ part2-2.js
@@ -0,0 +1,5 @@
+let name = 'mosh'; // Strain Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName = undefined;
+let lastName = null;   
---
+++ part2.js
@@ -0,0 +1,6 @@
+let name = 'mosh';
+console.log(name);
+
+let interestRate = 0.3;
+interesRate = 1;
+console.log(interestRate);
